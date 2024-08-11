f7cebc82e6ddd725bd0368cf0d3f8c0fe294e889
Falih TH <falihtaufiqul@gmail.com>
2024-08-11 16:09:25 +0700

    fix : solve conflicts

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,299 @@
+import React from 'react';
+import * as S from './Card.style';
+
+const Card = () => {
+  return (
+    <S.ScrollableContainer>
+      <S.MainCard>
+        <S.CardTitleBox>
+          <S.TextTitle>
+            Title
+          </S.TextTitle>
+        </S.CardTitleBox>
+        <S.CardCategory>
+          Category
+        </S.CardCategory>
+        <S.Date>
+          Date
+        </S.Date>
+        <S.Author>
+          Author
+        </S.Author>
+        <S.Likeborder>
+          <S.Liketitle>
+            Number of Likes
+            <S.NumberLike>
+              200+
+            </S.NumberLike>
+          </S.Liketitle>
+          <S.LogoHeart src="heart-fill.png" alt="logo"/>
+        </S.Likeborder>
+      </S.MainCard>
+
+      <S.MainCard>
+        <S.CardTitleBox>
+          <S.TextTitle>
+            Title 
+          </S.TextTitle>
+        </S.CardTitleBox>
+        <S.CardCategory>
+          Category
+        </S.CardCategory>
+        <S.Date>
+          Date 
+        </S.Date>
+        <S.Author>
+          Author
+        </S.Author>
+        <S.Likeborder>
+          <S.Liketitle>
+            Number of Likes
+            <S.NumberLike>
+              150+
+            </S.NumberLike>
+          </S.Liketitle>
+          <S.LogoHeart src="heart-fill.png" alt="logo"/>
+        </S.Likeborder>
+      </S.MainCard>
+
+      <S.MainCard>
+        <S.CardTitleBox>
+          <S.TextTitle>
+            Title
+          </S.TextTitle>
+        </S.CardTitleBox>
+        <S.CardCategory>
+          Category
+        </S.CardCategory>
+        <S.Date>
+          Date
+        </S.Date>
+        <S.Author>
+          Author
+        </S.Author>
+        <S.Likeborder>
+          <S.Liketitle>
+            Number of Likes
+            <S.NumberLike>
+              200+
+            </S.NumberLike>
+          </S.Liketitle>
+          <S.LogoHeart src="heart-fill.png" alt="logo"/>
+        </S.Likeborder>
+      </S.MainCard>
+
+      <S.MainCard>
+        <S.CardTitleBox>
+          <S.TextTitle>
+            Title
+          </S.TextTitle>
+        </S.CardTitleBox>
+        <S.CardCategory>
+          Category
+        </S.CardCategory>
+        <S.Date>
+          Date
+        </S.Date>
+        <S.Author>
+          Author
+        </S.Author>
+        <S.Likeborder>
+          <S.Liketitle>
+            Number of Likes
+            <S.NumberLike>
+              200+
+            </S.NumberLike>
+          </S.Liketitle>
+          <S.LogoHeart src="heart-fill.png" alt="logo"/>
+        </S.Likeborder>
+      </S.MainCard>
+
+      <S.MainCard>
+        <S.CardTitleBox>
+          <S.TextTitle>
+            Title
+          </S.TextTitle>
+        </S.CardTitleBox>
+        <S.CardCategory>
+          Category
+        </S.CardCategory>
+        <S.Date>
+          Date
+        </S.Date>
+        <S.Author>
+          Author
+        </S.Author>
+        <S.Likeborder>
+          <S.Liketitle>
+            Number of Likes
+            <S.NumberLike>
+              200+
+            </S.NumberLike>
+          </S.Liketitle>
+          <S.LogoHeart src="heart-fill.png" alt="logo"/>
+        </S.Likeborder>
+      </S.MainCard>
+
+      <S.MainCard>
+        <S.CardTitleBox>
+          <S.TextTitle>
+            Title
+          </S.TextTitle>
+        </S.CardTitleBox>
+        <S.CardCategory>
+          Category
+        </S.CardCategory>
+        <S.Date>
+          Date
+        </S.Date>
+        <S.Author>
+          Author
+        </S.Author>
+        <S.Likeborder>
+          <S.Liketitle>
+            Number of Likes
+            <S.NumberLike>
+              200+
+            </S.NumberLike>
+          </S.Liketitle>
+          <S.LogoHeart src="heart-fill.png" alt="logo"/>
+        </S.Likeborder>
+      </S.MainCard>
+
+      <S.MainCard>
+        <S.CardTitleBox>
+          <S.TextTitle>
+            Title
+          </S.TextTitle>
+        </S.CardTitleBox>
+        <S.CardCategory>
+          Category
+        </S.CardCategory>
+        <S.Date>
+          Date
+        </S.Date>
+        <S.Author>
+          Author
+        </S.Author>
+        <S.Likeborder>
+          <S.Liketitle>
+            Number of Likes
+            <S.NumberLike>
+              200+
+            </S.NumberLike>
+          </S.Liketitle>
+          <S.LogoHeart src="heart-fill.png" alt="logo"/>
+        </S.Likeborder>
+      </S.MainCard>
+
+      <S.MainCard>
+        <S.CardTitleBox>
+          <S.TextTitle>
+            Title
+          </S.TextTitle>
+        </S.CardTitleBox>
+        <S.CardCategory>
+          Category
+        </S.CardCategory>
+        <S.Date>
+          Date
+        </S.Date>
+        <S.Author>
+          Author
+        </S.Author>
+        <S.Likeborder>
+          <S.Liketitle>
+            Number of Likes
+            <S.NumberLike>
+              200+
+            </S.NumberLike>
+          </S.Liketitle>
+          <S.LogoHeart src="heart-fill.png" alt="logo"/>
+        </S.Likeborder>
+      </S.MainCard>
+
+      <S.MainCard>
+        <S.CardTitleBox>
+          <S.TextTitle>
+            Title
+          </S.TextTitle>
+        </S.CardTitleBox>
+        <S.CardCategory>
+          Category
+        </S.CardCategory>
+        <S.Date>
+          Date
+        </S.Date>
+        <S.Author>
+          Author
+        </S.Author>
+        <S.Likeborder>
+          <S.Liketitle>
+            Number of Likes
+            <S.NumberLike>
+              200+
+            </S.NumberLike>
+          </S.Liketitle>
+          <S.LogoHeart src="heart-fill.png" alt="logo"/>
+        </S.Likeborder>
+      </S.MainCard>
+
+      <S.MainCard>
+        <S.CardTitleBox>
+          <S.TextTitle>
+            Title
+          </S.TextTitle>
+        </S.CardTitleBox>
+        <S.CardCategory>
+          Category
+        </S.CardCategory>
+        <S.Date>
+          Date
+        </S.Date>
+        <S.Author>
+          Author
+        </S.Author>
+        <S.Likeborder>
+          <S.Liketitle>
+            Number of Likes
+            <S.NumberLike>
+              200+
+            </S.NumberLike>
+          </S.Liketitle>
+          <S.LogoHeart src="heart-fill.png" alt="logo"/>
+        </S.Likeborder>
+      </S.MainCard>
+
+
+      <S.MainCard>
+        <S.CardTitleBox>
+          <S.TextTitle>
+            Title 
+          </S.TextTitle>
+        </S.CardTitleBox>
+        <S.CardCategory>
+          Category
+        </S.CardCategory>
+        <S.Date>
+          Date
+        </S.Date>
+        <S.Author>
+          Author
+        </S.Author>
+        <S.Likeborder>
+          <S.Liketitle>
+            Number of Likes
+            <S.NumberLike>
+              150+
+            </S.NumberLike>
+          </S.Liketitle>
+          <S.LogoHeart src="heart-fill.png" alt="logo"/>
+        </S.Likeborder>
+      </S.MainCard>
+    </S.ScrollableContainer>
+    
+    
+  );
+};
+
+export default Card;
